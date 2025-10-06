e7e563f10b3a86016d49e0c5cf46bd1cf97a9f08
Vibhu <vikram.kr.gupta01@gmail.com>
2025-10-06 22:20:48 +0530

    some chnages on server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,118 +1,102 @@
-console.log("server is running hello!!")
+// todo to do
+// 1. add zod
+// 2. add hashed password
+// 3. check the singup and singin route on postman
 
+import prisma from "@workspace/db/client";
 import express from "express";
 import cors from "cors";
-
-
+import jwt from "jsonwebtoken";
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.post("api/v1/signup", (req, res)=> {
+app.post("api/v1/signup", async (req, res) => {
+  const { email, name, username, password } = req.body;
 
-const {email, name , username , image , password }= req.body;
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
 
+  if (existingUser) {
+    res.status(400).json({
+      message: "user already exists Please Login ",
+    });
+    return;
+  }
 
+  await prisma.user.create({
+    data: {
+      email,
+      name,
+      username,
+      password,
+    },
+  });
 
-const post = await db.user.create({
-data :{
-email,
-name ,
-username ,
-password,
-image,
-}
-
-})
-
-res.status(200).json({
-message :"user signup Done"
-})
-
-}
-)
-
-
-app.post("api/v1/signin", (req, res)=> {
-
-const { username ,  password }= req.body;
-
-
-
-const  existingUser  = await db.user.find({
-
-where :{
-username
-}
-})
-
-if(!existingUser){
-res.status(400).json({
-
-message:"user Not Exists please Signup first"
-
-})
-}
-
-// check password 
-
-if(existingUser.password !== password ){
-res.status(400).json({
-
-message:"password not correct"
-
-})
-} 
-
-// generate token 
-
-const token = jwt.sign()
-res.status(200).json({
-message :"user signup Done"
-})
-
-}
-)
-
-
-
-
-
-
-
-app.post("api/v1/post", (req, res)=> {
-
-const {title , description , image ,placeName , address, location }= req.body;
-
-const post = await db.post.create({
-data :{
-
-title ,
-description ,
-image, 
-placeName ,
-address, 
-location,
-}
-
-})
-
-
-}
-)
-
-
-app.get("/", (req, res)=> {
-res.send("Hello server")
-}
-)
-
-
-
-app.listen(PORT, ()=> {
-console.log(`Server is running on port : http://localhost:${PORT}`)
+  res.status(200).json({
+    message: "user signup Done",
+  });
 });
 
+app.post("api/v1/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (!existingUser) {
+    res.status(400).json({
+      message: "user Not Exists please Signup first",
+    });
+    return;
+  }
+
+  // check password
+
+  if (existingUser.password !== password) {
+    res.status(400).json({
+      message: "password not correct",
+    });
+  }
+
+  // generate token
+
+  const token = jwt.sign(existingUser.id as unknown as string, "ilovekiyara");
+
+  res.status(200).json({
+    message: "user signin Done",
+    token,
+  });
+});
+
+app.post("api/v1/post", async (req, res) => {
+  const { title, description, image, placeName, address, location } = req.body;
+
+  // const post = await prisma.post.create({
+  //   data: {
+  //     title,
+  //     description,
+  //     image,
+  //     placeName,
+  //     address,
+  //     location,
+  //   },
+  // });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port : http://localhost:${PORT}`);
+});
